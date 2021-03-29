bbc2f55426fa12a0d36016c26d81615ff61d6a1c
Diego Basurco <d24reyes@gmail.com>
2021-03-28 21:39:15 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+
+namespace sample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            countRabbitCouple(39);
+            
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void countRabbitCouple(int position){
+            int rabbitCount = 50;
+            ArrayList rabbitList = new ArrayList();
+
+            for(int i = 1; i < rabbitCount; i++){
+                if(i <= 2){
+                    rabbitList.Add(1);
+                }else{
+                    int count = rabbitList.Count;
+                    int sum = Int32.Parse(string.Format($"{rabbitList[rabbitList.Count-1]}")) + 
+                    Int32.Parse(string.Format($"{rabbitList[rabbitList.Count-2]}"));
+                    rabbitList.Add(sum);
+                }
+            }
+
+            Console.WriteLine(rabbitList[position-1]);
+        }
+    }
+}
